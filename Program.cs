94fccb17cb1c6539e97914647e50be1b007426cc
Dimitry-P <dimitry.piatnitsky@yahoo.com>
2024-12-23 16:53:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10
+{
+    //•	Задача 10: Напишите программу, которая выводит количество цифр в числе.
+    internal class Program
+    {
+        public static string num;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            num = Console.ReadLine();
+            Console.WriteLine("Количество цифр: "+ num.Length);
+        }
+    }
+}
